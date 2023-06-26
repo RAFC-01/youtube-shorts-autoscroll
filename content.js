71cb7bb712275aa3616917f0b00729f23d8d89f4
RAFi18 <PolskiRafiii@gmail.com>
2023-06-26 17:33:54 +0200

    short progress bar always visible

--- content.js
+++ content.js
@@ -18,15 +18,21 @@ let interval;
 function toggleCheck(){
     console.log('toggle check')
     if (document.location.href.includes('shorts')){
-        console.log(document.location.href);
+        let currentVidTag = ".reel-video-in-sequence[is-active]";
+        let progress_bar = document.querySelector(` ${currentVidTag} > .overlay > .style-scope > #overlay > #progress-bar > .style-scope > .style-scope`);
+        progress_bar.removeAttribute('hidden');
+        //auto scroll
         let lastRecordedValue = 0;
+        let lastRecordedURL = document.location.href.substring(document.location.href.lastIndexOf('shorts/')+7);
         if (interval) return;
         interval = setInterval(()=> {
-            let currentValue = +document.querySelector("#progress-bar-line").getAttribute('aria-valuenow')
-            if (lastRecordedValue > currentValue){
-                nextShort();
+            let currentValue = +document.querySelector("#progress-bar-line").getAttribute('aria-valuenow');
+            let currURL = document.location.href.substring(document.location.href.lastIndexOf('shorts/')+7);
+            if (lastRecordedValue > currentValue || currentValue == 100){
+                if (currURL == lastRecordedURL) nextShort();
             }
-            console.log(currentValue, lastRecordedValue);
+            console.log(lastRecordedURL);
+            lastRecordedURL = currURL;
             lastRecordedValue = currentValue;
         }, 500);
     }else{
