373f74c278247f4681e94e7f4ede4f2b6b8c7c6f
RAFi18 <PolskiRafiii@gmail.com>
2023-06-26 23:57:57 +0200

    you can skip one sec forward and back with arrowkeys

--- content.js
+++ content.js
@@ -1,44 +1,45 @@
+let videoPlayerTag;
+let timeSkipAmmount = 1;
 // check url change
 function urlChangeListener(){
     let last;
+    let isShorts = false;
     setInterval(()=> {
         let curr = document.location.href;
-        if (last && curr !== last) toggleCheck();
-        last = curr; 
+        let isCurrShorts = document.location.href.includes('shorts');
+        // console.log(isCurrShorts)
+        if (last && curr !== last) toggleCheck(isCurrShorts); // all url change
+        if (isCurrShorts !== isShorts) onShortsLocationChange(isCurrShorts); // just entering/exiting shorts
+        last = curr;
+        isShorts = isCurrShorts;
     },100);
 }
     
 function nextShort(){
     document.querySelector("#navigation-button-down > ytd-button-renderer > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill").click();
+    document.querySelector('.reel-video-in-sequence[is-active] > #player-container > #player > #container > div > div > video').removeEventListener('ended', nextShort);
 }
 
-let interval;
-function toggleCheck(){
-    if (document.location.href.includes('shorts')){
+function toggleCheck(isShorts){
+    if (isShorts){
+        // progress-bar
         let currentVidTag = ".reel-video-in-sequence[is-active]";
         waitForElem(` ${currentVidTag} > .overlay > .style-scope > #overlay > #progress-bar > .style-scope > .style-scope`, (elem)=> {
             elem.removeAttribute('hidden');
         });
-        let views = document.querySelector("#factoids > ytd-factoid-renderer:nth-child(2) > div").getAttribute('aria-label');
-        console.log(views);
+        // let views = document.querySelector("#factoids > ytd-factoid-renderer:nth-child(2) > div").getAttribute('aria-label');
+        // console.log(views);
         //auto scroll
-        // let videoPlayer = document.querySelector(`${currentVidTag} > #player-container > #player > #container > div > div > video`);
+        videoPlayerTag = `${currentVidTag} > #player-container > #player > #container > div > div > video`;
         let lastRecordedValue = 0;
-        let lastRecordedURL = document.location.href.substring(document.location.href.lastIndexOf('shorts/')+7);
+        let currURL = document.location.href.substring(document.location.href.lastIndexOf('shorts/')+7);
+        let vid;
         waitForElem(`${currentVidTag} > #player-container > #player > #container > div > div > video`, (video)=> {
             console.log(video);
             video.loop = false;
-            video.addEventListener('ended', ()=>{
-                let currURL = document.location.href.substring(document.location.href.lastIndexOf('shorts/')+7);
-                if (currURL == lastRecordedURL) {
-                    video.removeEventListener('ended', ()=> {
-                        nextShort();
-                    });
-                }
-            }, false)
+            video.addEventListener('ended', nextShort);
         })
-    }else{
-        if (interval) clearInterval(interval);
+        // video.removeEventListener('ended', nextShort);
     }
 }
 function waitForElem(tag, next){
@@ -57,5 +58,23 @@ function waitForElem(tag, next){
         next(0);
     }
 }
+function onShortsLocationChange(isShorts){
+    if (isShorts){
+        document.addEventListener('keydown', skipShortTime);
+    }else{
+        document.removeEventListener('keydown', skipShortTime);
+    }
+}
+function skipShortTime(e){
+    waitForElem(videoPlayerTag, (video)=>{
+        let forward = e.key == 'ArrowRight';
+        if (forward){
+            video.currentTime += timeSkipAmmount;
+        }else{
+            video.currentTime -= timeSkipAmmount;
+        }
+    })
+}
 urlChangeListener();
-toggleCheck();
+toggleCheck(document.location.href.includes('shorts'));
+onShortsLocationChange(document.location.href.includes('shorts'));
